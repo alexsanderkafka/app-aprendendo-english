0494b3eb83b0c55f4f94b3574e6b645e3db8e974
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-25 10:28:20 -0300

    Criado o menu de navegação e configuração de cores

--- app/src/main/java/com/alex/teste/appenglish/MainActivity.java
+++ app/src/main/java/com/alex/teste/appenglish/MainActivity.java
@@ -1,14 +1,45 @@
 package com.alex.teste.appenglish;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.alex.teste.appenglish.fragments.BichosFragment;
+import com.alex.teste.appenglish.fragments.NumerosFragment;
+import com.alex.teste.appenglish.fragments.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Bichos", BichosFragment.class)
+                .add("Números", NumerosFragment.class)
+                .add("Vogais", VogaisFragment.class)
+                .create());
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
     }
 }
